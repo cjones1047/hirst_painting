439bc274cb8dc014607cf41976468cdb25b7f9de
cjones1047 <cjmj1047@gmail.com>
2022-11-23 16:57:19 -0600

    30 rgb colors extracted from image.jpg using colorgram, grouped into an array of tuples

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import colorgram
 from turtle import Turtle, Screen, colormode
 
-colors = colorgram.extract('image.jpg', 15)
+# colors = colorgram.extract('image.jpg', 30)
+#
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-rgb_colors = []
-for color in colors:
-    rgb = color.rgb
-    rgb_colors.append(rgb)
-
-print(rgb_colors)
+rgb_colors = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
+              (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
+              (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
+              (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102),
+              (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
