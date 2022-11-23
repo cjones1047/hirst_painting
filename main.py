8bd0bda5c02321377720cc3ed245e032dbc6caab
cjones1047 <cjmj1047@gmail.com>
2022-11-23 16:45:02 -0600

    colorgram.py imported, 15 colors extracted from image.jpg

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import colorgram
+from turtle import Turtle, Screen, colormode
 
+colors = colorgram.extract('image.jpg', 15)
+
+rgb_colors = []
+for color in colors:
+    rgb = color.rgb
+    rgb_colors.append(rgb)
+
+print(rgb_colors)
