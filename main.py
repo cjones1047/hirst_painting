590be268e0183d178a178ea9fbcaaf8fb5c78d43
cjones1047 <cjmj1047@gmail.com>
2022-11-23 17:11:27 -0600

    Pointer successfully draws 100 dots sequentially, each dot choosing a random color from the rgb_colors list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import colorgram
+# import colorgram
 from turtle import Turtle, Screen, colormode
+import random
 
 # colors = colorgram.extract('image.jpg', 30)
 #
@@ -18,3 +19,20 @@ rgb_colors = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), 
               (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
               (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102),
               (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+turtle = Turtle()
+colormode(255)
+turtle.speed("fastest")
+turtle.penup()
+movement = 50
+dot = 20
+turtle.goto(-100, -100)
+position = turtle.position()
+
+for _ in range(100):
+    random_color = random.choice(rgb_colors)
+    turtle.dot(dot, random_color)
+    turtle.forward(movement)
+
+screen = Screen()
+screen.exitonclick()
